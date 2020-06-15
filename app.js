f572ca241078bdd7efc0f22c2cdf7075aebbe90c
uchchhwash <uchchhwash.git@gmail.com>
2020-06-15 18:28:16 +0600

    module export

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const date = require (__dirname + "/date.js");
 const app = express();
 
 let item = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = []
+let workItems = [];
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
 app.get("/", function (req, res) {
-    let today = new Date();
-
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    }
-
-    let day = today.toLocaleDateString("en-US",options);
+    let day = date();    
     res.render('list', { listTitle: day, listItem:item });
 })
 
