986abd9f52becc71593cf33c2ca79bbe13d8753c
uchchhwash <uchchhwash.git@gmail.com>
2020-06-15 13:52:45 +0600

    ejs add

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res){
-    res.send("Hello API");
+app.set('view engine', 'ejs');
+
+app.get("/", function (req, res) {
+    var today = new Date();
+    var curretDay = today.getDay();
+    var day = "";
+
+    if (curretDay === 6 || curretDay === 0) {
+        day = "Weekend";
+    }
+    else {
+        day = "Weekday";
+    }
+    res.render('list', { kindOfDay: day });
 })
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("server started on : http://localhost:3000");
 })
