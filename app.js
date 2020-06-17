402cd8f50ef9f9c6122dd3ac0678f669569132b7
uchchhwash <uchchhwash.git@gmail.com>
2020-06-17 21:35:13 +0600

    models style & ejs change

--- app.js
+++ app.js
@@ -1,43 +1,17 @@
 const express = require("express");
+
 const bodyParser = require("body-parser");
-const date = require (__dirname + "/date.js");
+const _ = require("lodash");
+const { getItems } = require("./models/models");
+const model = require(__dirname + "/models/models.js");
+
 const app = express();
 
-let item = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
 app.set('view engine', 'ejs');
 
-app.get("/", function (req, res) {
-    let day = date.getDate();
-    res.render('list', { listTitle: day, listItem:item });
-})
 
-app.post("/", function(req, res){
-    if(req.body.list === "Work"){
-        workItems.push(req.body.newItem)
-        res.redirect("/work");
-
-    }
-    else{
-        item.push(req.body.newItem);
-        res.redirect("/");
-    }
-
-})
-
-app.get("/work", function(req, res){
-    res.render("list", {listTitle: "Work List", listItem: workItems});
-})
-
-app.post('/work', function(req, res){
-    let item = req.body.newItem;
-    workItems.push(item);
-    res.redirect("/work");
-})
-app.listen(3000, function () {
+app.listen(3000, function() {
     console.log("server started on : http://localhost:3000");
 })
-
---
+++ models/models.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+mongooseDatabaseUrl = "mongodb://localhost:27017/todolistDB"
+
+mongoose.connect(mongooseDatabaseUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+
+let item = [];
+const itemSchema = {
+    name: String,
+}
+const Item = mongoose.model("Item", itemSchema);
+
+
+const listSchema = {
+    name: String,
+    items: [itemSchema]
+}
+const List = mongoose.model("List", listSchema);
+
+
+const item1 = new Item({
+    name: "Add an Item"
+})
+const item2 = new Item({
+    name: "Update an Item"
+})
+const item3 = new Item({
+    name: "Remove a Item"
+})
+let defaultItems = [item1, item2, item3]
+
+exports.defaultItems = defaultItems;
+exports.Item = Item;
+exports.List = List;
